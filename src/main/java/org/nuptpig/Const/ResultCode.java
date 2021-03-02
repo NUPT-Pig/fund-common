cd5fa582b2b00ad29e8ba26d6699e689f859f819
anshun_zhu <nupt_b09040212@sina.com>
2021-03-02 12:25:37 +0800

    init

---
+++ src/main/java/org/nuptpig/Const/ResultCode.java
@@ -0,0 +1,21 @@
+package org.nuptpig.Const;
+
+public enum ResultCode {
+    SUCCESS(200, "成功"),
+    FAIL(400, "失败");
+    private long code;
+    private String message;
+
+    ResultCode(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public long getCode(){
+        return this.code;
+    }
+
+    public String getMessage(){
+        return this.message;
+    }
+}
---
+++ src/main/java/org/nuptpig/Response/CommonResult.java
@@ -0,0 +1,33 @@
+package org.nuptpig.Response;
+
+import lombok.Data;
+import org.nuptpig.Const.ResultCode;
+
+@Data
+public class CommonResult<T> {
+    private long code;
+    private String message;
+    private T data;
+
+    private CommonResult(long code, String message, T data) {
+        this.code = code;
+        this.message = message;
+        this.data = data;
+    }
+
+    static <T> CommonResult<T> success(T data){
+        return new CommonResult<>(ResultCode.SUCCESS.getCode(), ResultCode.SUCCESS.getMessage(), data);
+    }
+
+    static <T> CommonResult<T> success(T data, ResultCode resultCode){
+        return new CommonResult<>(resultCode.getCode(), resultCode.getMessage(), data);
+    }
+
+    static <T> CommonResult<T> fail(T data){
+        return new CommonResult<>(ResultCode.FAIL.getCode(), ResultCode.FAIL.getMessage(), data);
+    }
+
+    static <T> CommonResult<T> fail(T data, ResultCode resultCode){
+        return new CommonResult<>(resultCode.getCode(), resultCode.getMessage(), data);
+    }
+}
